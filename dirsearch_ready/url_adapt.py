0acccde82081ea8982a435eada4f4f60649a56a1
Sicarius <42030228+ElSicarius@users.noreply.github.com>
2021-06-27 17:52:48 +0200

    Update url_adapt.py

--- dirsearch_ready/url_adapt.py
+++ dirsearch_ready/url_adapt.py
@@ -3,11 +3,13 @@
 
 import os
 import sys
+import re
 from urllib.parse import urlparse, urlunparse
 
 for url in sys.stdin.readlines():
     url = urlparse(url)
-    path = url.path
-    if len(path.split("/"))>=1:
-        path = os.path.join("/", *os.path.split(path)[:-1])
-    sys.stdout.write(urlunparse(url._replace(path=path, query=""))+"\n")
+    path = os.path.split(url.path)
+    if len(path) >= 1 and re.findall("[^.]+[a-zA-Z\d]+", str(path[-1])):
+        path = path[:-1]
+    path = os.path.join("/", *path)
+    sys.stdout.write(urlunparse(url._replace(path=path))+"\n")
